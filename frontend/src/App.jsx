d1eb1c679dc8feb9b081ffe514df40188b772b12
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2026-05-03 13:26:55 +0530

    feat: add routing for subscriptions and alerts pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Members from "./pages/Members";
+import Subscriptions from "./pages/Subscriptions";
+import SubscriptionList from "./pages/SubscriptionList";
+import Alerts from "./pages/Alerts";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/members" element={<Members />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/subscriptions-list" element={<SubscriptionList />} />
+        <Route path="/alerts" element={<Alerts />} />
       </Routes>
     </BrowserRouter>
   );
