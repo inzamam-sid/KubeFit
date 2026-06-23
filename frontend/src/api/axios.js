e5e84edd519eab90bebeb963dd157a33fcd8a28d
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2026-06-23 17:27:53 +0530

    feat: Update API base URL to use production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://kubefit.onrender.com/api",
 });
 
 // Attach access token
